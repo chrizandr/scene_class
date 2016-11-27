fba5692088a4a0aa10460b258099b5b9233a649a
chrizandr <chris.g14@iiits.in>
2016-11-27 18:00:15 +0530

    Wrote Gabor filter and the image normalization source

---
+++ Gabor.py
@@ -0,0 +1,42 @@
+##########################################################################################
+## Scene Classification                                                                 ##
+## Authors : Chris Andrew, Santhoshini Reddy, Nikath Yasmeen, Sai Hima, Sriya Ragini    ##
+###################################################################                     ##
+## Description: This project was developed as part of the DIP course at IIIT Sri City   ##
+## All code is available for free usage for educational purposes                        ##
+## Authors do not authorize commercial use of the source code                           ##
+##########################################################################################
+
+################ Imports ################
+from cv2 import getGaussianKernel
+import numpy as np
+################ Source ################
+
+# makeGaborKernel creates two Gabor filters of opposite symmetry to be used for Gabor filtering of the image
+# makeGaborKernel(gkernel , f , theta) -> he , ho
+# gkernel : numpy 2D array that contains the gaussian kernel that is to be used to create the filters
+# f : frequency of the filters
+# theta : orientation of the filter
+# he , ho : the output filters coresponding to opposite symmetry filters (he : sin wave filter , ho : cosine wave filter )
+
+
+def makeGaborKernel(gkernel,f,theta):
+    he=np.arange(gkernel.size).reshape(gkernel.shape)
+    ho=np.arange(gkernel.size).reshape(gkernel.shape)
+    he=he.astype(np.float64)
+    ho=ho.astype(np.float64)
+    for i in range(he.shape[0]):
+        for j in range(he.shape[1]):
+            val= 2*np.pi*f*((i+1)*np.cos(theta)+(j+1)*np.sin(theta))
+            he[i,j]=gkernel[i,j]*np.cos(val)
+    for i in range(ho.shape[0]):
+        for j in range(ho.shape[1]):
+            val= 2*np.pi*f*((i+1)*np.cos(theta)+(j+1)*np.sin(theta))
+            ho[i,j]=gkernel[i,j]*np.sin(val)
+    return he,ho
+
+def makeGaussianKernel(ksize, sigma):
+    kx=getGaussianKernel(ksize[0],sigma);
+    ky=getGaussianKernel(ksize[1],sigma);
+    kernel = kx*ky.transpose()
+    return kernel
---
+++ normalize.py
@@ -0,0 +1,43 @@
+##########################################################################################
+## Scene Classification                                                                 ##
+## Authors : Chris Andrew, Santhoshini Reddy, Nikath Yasmeen, Sai Hima, Sriya Ragini    ##
+###################################################################                     ##
+## Description: This project was developed as part of the DIP course at IIIT Sri City   ##
+## All code is available for free usage for educational purposes                        ##
+## Authors do not authorize commercial use of the source code                           ##
+##########################################################################################
+
+################ Imports ################
+import cv2
+################ Source ################
+
+# For illumination normalization we decided to go with global histogram equilization
+# Local histogram equilization can also be acheived for the image, which is more useful in retaining fine features
+# Since we were mostly interested in the structure of the image rather than the finer details in it, we went for global equilization
+# The function normalizeImage does both local and global equilization based on the whether the flag parameter is set to "g" or "l"
+
+# normalizeImage(img , flag) -> result
+# img : input image
+# flag : flag to identify the type of normalization to be done (flag in ['g','l'])
+# result : normalized image
+
+def normalizeImage(img, flag):
+        r_img = None
+        if flag == 'g':
+            r_img = cv2.equalizeHist(img)
+        elif flag == 'l':
+            clahe = cv2.createCLAHE()
+            r_img = clahe.apply(img)
+        return r_img
+
+# Here is an example of how to use the function
+# img = cv2.imread("test.jpg",0)
+#
+# g_img = normalizeImage(img,'g')
+# l_img = normalizeImage(img,'l')
+#
+# cv2.imshow("Original",img)
+# cv2.imshow("Global",g_img)
+# cv2.imshow("Local",l_img)
+# cv2.waitKey(0)
+# cv2.destroyAllWindows()
